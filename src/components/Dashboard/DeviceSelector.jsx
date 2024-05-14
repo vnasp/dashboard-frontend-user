52546173040578431601f5c85732320a0c2c127d
Valentina <valentinamr@gmail.com>
2024-05-13 20:18:10 -0400

    Update selectors

--- src/components/Dashboard/DeviceSelector.jsx
+++ src/components/Dashboard/DeviceSelector.jsx
@@ -1,54 +1,93 @@
-import { useState } from "react";
-import stations from "../../data/DataExample";
+import stations from '../../data/StationsData';
 
-function DeviceSelector() {
-  const [selectedStation, setSelectedStation] = useState("");
-  const [selectedDevice, setSelectedDevice] = useState("");
+import { useState, useEffect } from 'react';
+import Select from 'react-select';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { startOfToday, subDays, subWeeks, subMonths } from 'date-fns';
 
-  // Actualización dinámica de dispositivos por estación
-  const devices = selectedStation
-    ? stations.find((station) => station.id === parseInt(selectedStation))
-        .devices
-    : [];
+export default function DeviceSelector() {
+    const [selectedStations, setSelectedStations] = useState([]);
+    const [selectedDevices, setSelectedDevices] = useState([]);
+    const [selectedTimeRange, setSelectedTimeRange] = useState(null);
+    const [customDateRange, setCustomDateRange] = useState([null, null]);
+    const [startDate, endDate] = customDateRange;
 
-  return (
-    <section className="flex justify-evenly">
-      <div className="space-x-4">
-        <label htmlFor="station-select">Selecciona una estación:</label>
-        <select
+    useEffect(() => {
+        // Actualiza los dispositivos basado en las estaciones seleccionadas
+        const allSelectedDevices = selectedStations.flatMap(station =>
+            station.devices.map(device => ({ ...device, label: `Dispositivo ${device.id}` }))
+        );
+        setSelectedDevices(prevDevices =>
+            prevDevices.filter(device => allSelectedDevices.some(sDevice => sDevice.id === device.id))
+        );
+    }, [selectedStations]);
+
+    const handleStationChange = selectedOptions => {
+        setSelectedStations(selectedOptions);
+    };
+
+    const handleDeviceChange = selectedOptions => {
+        setSelectedDevices(selectedOptions);
+    };
+
+    const handleTimeRangeChange = selectedOption => {
+        setSelectedTimeRange(selectedOption);
+        if (selectedOption.id !== 'custom') {
+            setCustomDateRange([null, null]); 
+        }
+    };
+
+    const stationOptions = stations.map(station => ({
+        value: station.id,
+        label: `Estación ${station.id}`,
+        devices: station.devices
+    }));
+
+    const timeOptions = [
+      { id: 'today', label: 'Hoy', value: { start: startOfToday(), end: new Date() } },
+      { id: 'lastWeek', label: 'Última Semana', value: { start: subWeeks(startOfToday(), 1), end: new Date() } },
+      { id: 'last30Days', label: 'Últimos 30 días', value: { start: subDays(startOfToday(), 30), end: new Date() } },
+      { id: 'last90Days', label: 'Últimos 90 días', value: { start: subMonths(startOfToday(), 3), end: new Date() } },
+      { id: 'custom', label: 'Personalizado', value: null }
+  ];
+
+    return (
+      <section className="grid grid-cols-3 gap-6">
+      <Select
           id="station-select"
-          value={selectedStation}
-          onChange={(e) => {
-            setSelectedStation(e.target.value);
-            setSelectedDevice("");
-          }}
-        >
-          <option value="">Seleccione una estación</option>
-          {stations.map((station) => (
-            <option key={station.id} value={station.id}>
-              {station.name}
-            </option>
-          ))}
-        </select>
-      </div>
+          onChange={handleStationChange}
+          options={stationOptions}
+          placeholder="Seleccione estaciones"
+          isMulti
+      />
 
-      <div className="space-x-4">
-        <label htmlFor="device-select">Selecciona un dispositivo:</label>
-        <select
+      <Select
           id="device-select"
-          value={selectedDevice}
-          onChange={(e) => setSelectedDevice(e.target.value)}
-        >
-          <option value="">Todos los dispositivos</option>
-          {devices.map((device) => (
-            <option key={device.id} value={device.id}>
-              {device.name}
-            </option>
-          ))}
-        </select>
-      </div>
-    </section>
-  );
-}
+          onChange={handleDeviceChange}
+          options={selectedStations.flatMap(station => station.devices.map(device => ({ value: device.id, label: `Dispositivo ${device.id}` })))}
+          placeholder="Todos los dispositivos"
+          isMulti
+          isDisabled={!selectedStations.length}
+      />
 
-export default DeviceSelector;
+      <Select
+          id="time-range-select"
+          onChange={handleTimeRangeChange}
+          options={timeOptions}
+          placeholder="Seleccione un rango de tiempo"
+      />
+
+      {selectedTimeRange && selectedTimeRange.id === 'custom' && (
+          <DatePicker
+              selectsRange={true}
+              startDate={startDate}
+              endDate={endDate}
+              onChange={setCustomDateRange}
+              isClearable={true}
+              placeholderText="Selecciona un rango de fechas"
+          />
+      )}
+  </section>
+    );
+  }
