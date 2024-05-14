c441509cc7bba0be27b2d9ab2f44a37fb91eafe4
Valentina <valentinamr@gmail.com>
2024-05-14 02:52:10 -0400

    Updates

--- assets/utils-D0L4_1WI.js
+++
@@ -1,1 +0,0 @@
-import{l as a}from"./vendor-Cg58ssI3.js";const e=a.create({baseURL:"/acustica-frontend-user/",timeout:1e3,headers:{"Content-Type":"application/json",Accept:"application/json"}});export{e as a};
