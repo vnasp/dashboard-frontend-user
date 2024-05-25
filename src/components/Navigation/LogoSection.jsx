7211aaf46219a7137e30e707dd1aa4f86b63b45b
Valentina Muñoz <valentinamr@gmail.com>
2024-05-25 16:33:51 -0400

    Update LogoSection.jsx

--- src/components/Navigation/LogoSection.jsx
+++ src/components/Navigation/LogoSection.jsx
@@ -2,11 +2,11 @@ export default function LogoSection() {
   return (
     <>
       <section className="navigation__logo">
-        <a href="#" title="Acustica Marina">
+        <a href="#" title="Logo">
           <img
             src="./logo.webp"
             className="w-36"
-            alt="Acustina Marina"
+            alt="Logo"
             width={100}
             height={100}
           />
